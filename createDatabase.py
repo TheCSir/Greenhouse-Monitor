87f78a3df7af7d54d8b4cf4f65595b1bb2a6cd19
acnacor <acnacor@gmail.com>
2019-03-26 19:50:45 +1100

    script to create table schema in db

---
+++ createDatabase.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+# Remember to install SQLite, create a directory and then create a database
+conn = lite.connect('sensehat.db')
+
+with conn:
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS SENSEHAT_data")
+    cur.execute(CREATE TABLE SENSEHAT_Data(timestamp DATETIME, temp NUMERIC, humidity NUMERIC"))
