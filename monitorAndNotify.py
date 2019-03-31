1e84de8c7bd8cdd1e0a38ba15078c0676323614f
acnacor <acnacor@gmail.com>
2019-03-31 20:08:48 +1100

    Added push notification feature

--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -3,11 +3,15 @@ from sense_hat import SenseHat
 import time
 import sqlite3
 import json
+import requests
 
 class MonitorAndNotify():
 
 # timezone = Offset in GMT e.g Melbourne timezone is GMT+11
 # dbname = name of the database
+    
+    # Put your accesstoken from Pushbullet here
+    ACCESS_TOKEN = ""
 
     def __init__(self, timezone=11,dbname='sensehat.db',senseHat=SenseHat()):
         self.temperature = senseHat.get_temperature()
@@ -34,13 +38,24 @@ class MonitorAndNotify():
         conn.commit()
         conn.close()
 
-        #after adding into database, check if need to push notification
+        #Check recorded temperature to see if needed to push notification
         self.checkDataBounds()
 
+    def send_notification(self,title,body):
+        data_send = {"type": "note", "title": title, "body": body}
+        response = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(data_send), headers={'Authorization': 'Bearer ' + self.ACCESS_TOKEN, 'Content-Type': 'application/json'})
+        
+        if response.status_code != 200:
+            raise Exception('Message not sent.')
+        
     def checkDataBounds(self):
         with open("config.json", "r") as file:
             data = json.load(file)
         
-        if self.temperature < data["min_temperature"] or self.temperature > data["max_temperature"] or self.humidity < data["min_humidity"] or self.humidity > data["max_humidity"]:
-            # Pushbullet notification
-            print("")
+        if self.temperature < data["min_temperature"] or self.humidity < data["min_humidity"]:
+            self.send_notification("Raspberry Pi Data Update", "Temperature and/or Humidity is less than the configured parameters.")
+        elif self.temperature > data["max_temperature"] or self.humidity > data["max_humidity"]:
+            self.send_notification("Raspberry Pi Data Update", "Temperature and/or Humidity is greater than configured parameters.")
+
+            
+
