f2c85579edf4f8bbe3c6124b447bec797fbcef62
Shehan Chathura <s3651907@student.rmit.edu.au>
2019-04-03 18:02:11 +1100

    Added comments to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from monitorAndNotify import MonitorAndNotify
 
 
+
+#init MoniMonitorAndNotify class with user inputs
 main = MonitorAndNotify(11,'sensehat.db','config.json',"o.ZJwli1lhTH9nH1pKrSisFfMivfGO3SP4")
 
+#call getSenseHatData method
 main.getSenseHatData()
