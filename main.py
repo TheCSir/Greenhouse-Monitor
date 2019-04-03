e30dd3e4c9a7f05e581e84ad373edaa22c567daa
Shehan Chathura <s3651907@student.rmit.edu.au>
2019-04-03 17:57:08 +1100

    Refined MonitorAndNotify.py removing hard code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from monitorAndNotify import MonitorAndNotify
-main = MonitorAndNotify()
+
+
+main = MonitorAndNotify(11,'sensehat.db','config.json',"o.ZJwli1lhTH9nH1pKrSisFfMivfGO3SP4")
 
 main.getSenseHatData()
--- monitorAndNotify.py
+++ monitorAndNotify.py
@@ -9,36 +9,43 @@ class MonitorAndNotify():
 
 # timezone = Offset in GMT e.g Melbourne timezone is GMT+11
 # dbname = name of the database
-    
-    # Put your accesstoken from Pushbullet here
-    ACCESS_TOKEN = "o.ZJwli1lhTH9nH1pKrSisFfMivfGO3SP4"
 
-    def __init__(self, timezone=11,dbname='sensehat.db',senseHat=SenseHat()):
-        self.temperature = senseHat.get_temperature()
-        self.humidity = senseHat.get_humidity()
+    def __init__(self, timezone,dbname,cofigfile,accesstoken):
+        self.temperature = None
+        self.humidity = None
         self.timezone= timezone
         self.dbname = dbname
-        self.senseHat = senseHat
+        self.configuration = cofigfile
+        self.ACCESS_TOKEN=accesstoken
     
     def getSenseHatData(self):
-        if self.temperature is not None and self.humidity is not None:
-            self.temperature = round(self.temperature, 1)
-            self.humidity = round(self.humidity, 1)
-            self.logData()   
+
+        #init SenseHat and read data
+        senseHat_call = SenseHat()
+        temperature = senseHat_call.get_temperature()
+        humidity = senseHat_call.get_humidity()
+
+        #set global variables
+        self.temperature = round(temperature, 1)
+        self.humidity = round(humidity, 1)
+        self.logData()   
     
+    #get current timestamp
     def getTime(self):
         currentTime = datetime.utcnow()
         currentTime = currentTime - timedelta(hours=self.timezone)
         return(currentTime.strftime("%H:%M"))
 
+    #get current date
     def getDate(self):
         currentDate = time.strftime("%d/%m/%Y")
         return(currentDate)
 
+    #add gathered data to Database
     def logData(self):
         conn = sqlite3.connect(self.dbname)
         curs = conn.cursor()
-        curs.execute("INSERT INTO SENSEHAT_data values(?,?,?,?)", (self.getDate, self.getTime(), self.temperature, self.humidity))
+        curs.execute("INSERT INTO SENSEHAT_data values(?,?,?,?)", (self.getDate(), self.getTime(), self.temperature, self.humidity))
         conn.commit()
         conn.close()
 
@@ -53,7 +60,7 @@ class MonitorAndNotify():
             raise Exception('Message not sent.')
         
     def checkDataBounds(self):
-        with open("config.json", "r") as file:
+        with open(self.configuration, "r") as file:
             data = json.load(file)
         
         if self.temperature < data["min_temperature"] or self.humidity < data["min_humidity"]:
