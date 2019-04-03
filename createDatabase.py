d8bdba693adbd406e281c6c0ed499ced12dca445
Shehan Chathura <s3651907@student.rmit.edu.au>
2019-04-03 20:25:07 +1100

    Add database for daily notification limit

--- createDatabase.py
+++ createDatabase.py
@@ -5,7 +5,13 @@ conn = sqlite3.connect('sensehat.db')
 
 with conn:
     cur = conn.cursor()
+
+    #table for data reads
     cur.execute("DROP TABLE IF EXISTS SENSEHAT_data")
     cur.execute("CREATE TABLE SENSEHAT_Data(date DATETIME, timestamp DATETIME, temp NUMERIC, humidity NUMERIC)")
+
+    #table for daily notifications
+    cur.execute("DROP TABLE IF EXISTS SENSEHAT_DailyNotification")
+    cur.execute("CREATE TABLE SENSEHAT_DailyNotification(date DATETIME)")
 
     
