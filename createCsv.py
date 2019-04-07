230c499ff426133b7e65b3aa6b8d4bb9c8a4268c
Shehan Chathura <s3651907@student.rmit.edu.au>
2019-04-07 20:58:04 +1000

    Execute order fix

--- createCsv.py
+++ createCsv.py
@@ -14,44 +14,55 @@ class CreateCSV:
     #get datafromDatabase
     def GenerateReport(self):
 
-        #open sql connection
-        conn = sqlite3.connect(self.dbName)
-        cur = conn.cursor()
+        try:
+            #open sql connection
+            conn = sqlite3.connect(self.dbName)
+            cur = conn.cursor()
 
-        #call init csv
-        self.InitCSV()
+            #call init csv
+            self.InitCSV()
 
-        #get all diffrent days
-        cur.execute("SELECT * FROM `SENSEHAT_Data`;")
+            #get all diffrent days
+            cur.execute("SELECT * FROM `SENSEHAT_Data`;")
+            result= cur.fetchall()
 
-        #loop throught days
-        result= cur.fetchall()
-        for row in result:
-            date = row[0]
-            time = row[1]
-            temperature = row[2]
-            humidity = row[3]
-            
-            #append data to csv
-            self.writeToCSV(date,time,temperature,humidity)
+            #loop throught days
+            for row in result:
+                date = row[0]
+                time = row[1]
+                temperature = row[2]
+                humidity = row[3]
+                
+                #append data to csv
+                self.writeToCSV(date,time,temperature,humidity)
+        except Exception as err:
+        print('Query Failed: %s\nError: %s' % (query, str(err)))
+
+        
 
     #append the data to csv
     def writeToCSV(self,date,time,temperature,humidity):
 
-        with open(self.outputFileName , 'a', newline='') as file:
+        try:
+            with open(self.outputFileName , 'a', newline='') as file:
 
-            #append data
-            writer = csv.writer(file,escapechar='', quoting=csv.QUOTE_NONE)
-            writer.writerow([date,time,temperature,humidity])
+                #append data
+                writer = csv.writer(file,escapechar='', quoting=csv.QUOTE_NONE)
+                writer.writerow([date,time,temperature,humidity])
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
     #init csv header
     def InitCSV(self):
-        
-        with open(self.outputFileName , 'w',newline='') as file:
 
-            #append initial row
-            writer = csv.writer(file)
-            writer.writerow(['Date','Time','Temperature','Humidity'])
+        try:
+            with open(self.outputFileName , 'w',newline='') as file:
+
+                #append initial row
+                writer = csv.writer(file)
+                writer.writerow(['Date','Time','Temperature','Humidity'])
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
 report = CreateCSV('Analytics/data.csv')
 report.GenerateReport()
